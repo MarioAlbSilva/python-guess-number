26d5077753ed007f440de44620cf61611a0994ce
MarioAlbSilva <marioalb95@gmail.com>
2020-08-14 16:34:25 -0500

    Adivinar el numero

--- application.py
+++ application.py
@@ -1,3 +1,27 @@
-# Aqui escribe tu codigo
+import random
 
-print "Hello World"
+print("JUEGO DE ADIVINAR UN NUMERO")
+print("El objetivo es adivinar un número entre 1 y 30.")
+print("Se tiene 8 intentos para adivinarlo.")
+
+numero_oculto = random.randint(1, 30)
+intentos = 8
+
+while intentos > 0:
+	try:
+		numero = int(input("Ingrese un número:"))
+
+		if numero == numero_oculto:
+			print("Correcto, ha ganado")
+			break
+		elif numero > numero_oculto:
+			print("El número ingresado es mayor.")
+		elif numero < numero_oculto:
+			print("El número ingresado es menor.")
+
+		intentos -= 1
+	except:
+		print("El valor no es un número.")
+
+if intentos == 0:
+	print("Se acabaron los intentos, juego terminado.")
