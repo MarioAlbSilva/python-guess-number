ed7ca96ede181d385052310a4c026d1dd3c09379
MarioAlbSilva <marioalbsilva95@gmail.com>
2020-08-14 17:01:45 -0500

    Opción para seguir jugando

--- application.py
+++ application.py
@@ -6,22 +6,38 @@ print("Se tiene 8 intentos para adivinarlo.")
 
 numero_oculto = random.randint(1, 30)
 intentos = 8
+continuar = True
 
-while intentos > 0:
-	try:
-		numero = int(input("Ingrese un número:"))
+while continuar:
+	while intentos > 0:
+		try:
+			numero = int(input("Ingrese un número:"))
 
-		if numero == numero_oculto:
-			print("Correcto, ha ganado")
+			if numero == numero_oculto:
+				print("Correcto, ha ganado")
+				break
+			elif numero > numero_oculto:
+				print("El número ingresado es mayor.")
+			elif numero < numero_oculto:
+				print("El número ingresado es menor.")
+
+			intentos -= 1
+		except:
+			print("El valor no es un número.")
+
+	if intentos == 0:
+		print("Se acabaron los intentos, juego terminado.")
+
+	opcion = ''
+
+	while opcion != 'Y' or opcion != 'N':
+		opcion = input("¿Desea volver a intentarlo? Y/N:")
+
+		if opcion.lower() == 'y':
+			numero_oculto = random.randint(1, 30)
+			intentos = 8
 			break
-		elif numero > numero_oculto:
-			print("El número ingresado es mayor.")
-		elif numero < numero_oculto:
-			print("El número ingresado es menor.")
-
-		intentos -= 1
-	except:
-		print("El valor no es un número.")
-
-if intentos == 0:
-	print("Se acabaron los intentos, juego terminado.")
+		elif opcion.lower() == 'n':
+			print("Gracias por jugar, vuelva pronto.")
+			continuar = False
+			break
